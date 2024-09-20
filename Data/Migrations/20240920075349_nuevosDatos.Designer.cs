a9ccf90828ab6d22dffa223a83551b72ee9a2b69
nikyuher <totalgamenico@gmail.com>
2024-09-20 10:05:41 +0200

    Nuevos Datos

--- Data/Migrations/20240920075349_nuevosDatos.Designer.cs
+++ Data/Migrations/20240920075349_nuevosDatos.Designer.cs
@@ -3,6 +3,7 @@ using Buscador.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Buscador.Data.Migrations
 {
     [DbContext(typeof(BuscadorContext))]
-    partial class BuscadorContextModelSnapshot : ModelSnapshot
+    [Migration("20240920075349_nuevosDatos")]
+    partial class nuevosDatos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
