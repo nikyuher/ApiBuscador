9670a069706a1e373d3ef7c30e941c9b857c6f50
nikyuher <totalgamenico@gmail.com>
2024-09-20 10:48:40 +0200

    Correcion errores

--- Data/Migrations/20240917122105_AtributosAgregados.Designer.cs
+++ Data/Migrations/20240917122105_AtributosAgregados.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Buscador.Data.Migrations
 {
     [DbContext(typeof(BuscadorContext))]
-    [Migration("20240920073546_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20240917122105_AtributosAgregados")]
+    partial class AtributosAgregados
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -242,7 +242,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 1,
                             Descripcion = "Líder mundial en distribución de moda.",
                             Direccion = "Av. de la Diputación, 15142 Arteijo, La Coruña",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/inditexLC.jpg?updatedAt=1726649179851",
+                            Imagen = "https://example.com/inditex.png",
                             Nombre = "Inditex"
                         },
                         new
@@ -250,7 +250,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 2,
                             Descripcion = "Una de las mayores entidades bancarias del mundo.",
                             Direccion = "Ciudad Grupo Santander, 28660 Boadilla del Monte, Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/santander.jpg?updatedAt=1726649877238",
+                            Imagen = "https://example.com/santander.png",
                             Nombre = "Banco Santander"
                         },
                         new
@@ -258,7 +258,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 3,
                             Descripcion = "Multinacional española de telecomunicaciones.",
                             Direccion = "Distrito Telefónica, 28050 Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/telefonica.jpg?updatedAt=1726649877081",
+                            Imagen = "https://example.com/telefonica.png",
                             Nombre = "Telefónica"
                         },
                         new
@@ -266,7 +266,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 4,
                             Descripcion = "Una de las mayores empresas energéticas del mundo.",
                             Direccion = "Plaza Euskadi 5, 48009 Bilbao",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/iberdrola-.jpg?updatedAt=1726649877427",
+                            Imagen = "https://example.com/iberdrola.png",
                             Nombre = "Iberdrola"
                         },
                         new
@@ -274,7 +274,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 5,
                             Descripcion = "Empresa energética multinacional española.",
                             Direccion = "Calle Méndez Álvaro, 44, 28045 Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/repsol.webp?updatedAt=1726649877458",
+                            Imagen = "https://example.com/repsol.png",
                             Nombre = "Repsol"
                         },
                         new
@@ -282,7 +282,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 6,
                             Descripcion = "Cadena de grandes almacenes más grande de Europa.",
                             Direccion = "Calle Hermosilla, 112, 28009 Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/elcorteingles.jpeg?updatedAt=1726649879087",
+                            Imagen = "https://example.com/elcorteingles.png",
                             Nombre = "El Corte Inglés"
                         },
                         new
@@ -290,7 +290,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 7,
                             Descripcion = "Uno de los bancos más importantes de España.",
                             Direccion = "Av. Diagonal, 621, 08028 Barcelona",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/caixabanck.jpg?updatedAt=1726649879170",
+                            Imagen = "https://example.com/caixabank.png",
                             Nombre = "CaixaBank"
                         },
                         new
@@ -298,7 +298,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 8,
                             Descripcion = "Fabricante de automóviles con sede en Martorell.",
                             Direccion = "Autovía A-2, Km 585, 08760 Martorell, Barcelona",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/seat.jpg?updatedAt=1726649876969",
+                            Imagen = "https://example.com/seat.png",
                             Nombre = "Seat"
                         },
                         new
@@ -306,7 +306,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 9,
                             Descripcion = "Gestión de aeropuertos en España y en el mundo.",
                             Direccion = "Calle Peonías, 12, 28042 Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/aena-aeropuerto.jpg?updatedAt=1726649877286",
+                            Imagen = "https://example.com/aena.png",
                             Nombre = "Aena"
                         },
                         new
@@ -314,7 +314,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 10,
                             Descripcion = "Empresa global de infraestructuras y servicios.",
                             Direccion = "Calle Príncipe de Vergara, 135, 28002 Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/ferrovial.jpeg?updatedAt=1726649879406",
+                            Imagen = "https://example.com/ferrovial.png",
                             Nombre = "Ferrovial"
                         },
                         new
@@ -322,7 +322,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 11,
                             Descripcion = "Compañía de seguros global.",
                             Direccion = "Carretera de Pozuelo, 52, 28220 Majadahonda, Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/mafre.jpg?updatedAt=1726649877454",
+                            Imagen = "https://example.com/mapfre.png",
                             Nombre = "Mapfre"
                         },
                         new
@@ -330,7 +330,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 12,
                             Descripcion = "Líder en energías renovables y construcción sostenible.",
                             Direccion = "Avenida de Europa, 18, 28108 Alcobendas, Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/acciona.webp?updatedAt=1726649877393",
+                            Imagen = "https://example.com/acciona.png",
                             Nombre = "Acciona"
                         },
                         new
@@ -338,7 +338,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 13,
                             Descripcion = "Multinacional de banca y servicios financieros.",
                             Direccion = "Calle Azul, 4, 28050 Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/BBVA.jpg?updatedAt=1726649879175",
+                            Imagen = "https://example.com/bbva.png",
                             Nombre = "BBVA"
                         },
                         new
@@ -346,7 +346,7 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 14,
                             Descripcion = "Proveedor líder de tecnología en la industria del viaje.",
                             Direccion = "Calle Salvador de Madariaga, 1, 28027 Madrid",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/amadeus.jpg?updatedAt=1726649879691",
+                            Imagen = "https://example.com/amadeus.png",
                             Nombre = "Amadeus"
                         },
                         new
@@ -354,15 +354,15 @@ namespace Buscador.Data.Migrations
                             IdEmpresa = 15,
                             Descripcion = "Líder global en el sector de los hemoderivados.",
                             Direccion = "Calle Jesús i Maria, 6, 08022 Barcelona",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/grifols.jpg?updatedAt=1726649877212",
+                            Imagen = "https://example.com/grifols.png",
                             Nombre = "Grifols"
                         },
                         new
                         {
-                            IdEmpresa = 16,
+                            IdEmpresa = 30,
                             Descripcion = "Una de las mayores marcas de moda del mundo.",
                             Direccion = "Av. de la Diputación, 15142 Arteijo, La Coruña",
-                            Imagen = "https://ik.imagekit.io/Mariocanizares/Empresas/zara.jpg?updatedAt=1726649877219",
+                            Imagen = "https://example.com/zara.png",
                             Nombre = "Zara"
                         });
                 });
--- Data/Readme.txt
+++
@@ -1,4 +0,0 @@
-docker-compose up --build --force-recreate -d
-dotnet ef migrations add InitialCreate -p Data -s Api
-dotnet ef database update -p Data -s Api
-dotnet ef database drop -p Data -s Api
